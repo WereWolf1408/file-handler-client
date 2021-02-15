45a0f9863b772402a482b04968d2f1d71779b1ce
Anton Fiadotau <Anton_Fiadotau@epam.com>
2021-02-15 22:49:41 +0300

    create 'testAPI' component

---
+++ src/components/TestComponent.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class TestAPIComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Test API</h2>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default TestAPIComponent;
